477d683af0aa31034af77b5f2cbaad8e629a714f
DESKTOP-A6KOF2G\youbi <youbinking@naver.com>
2022-07-31 11:29:48 +0900

    문자열 길이

---
+++ Baekjoon_7/JAVA/Q2743.java
@@ -0,0 +1,14 @@
+package Baekjoon.JAVA;
+
+import java.util.Scanner;
+
+public class Q2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        System.out.println(input.length());
+
+    }
+}
