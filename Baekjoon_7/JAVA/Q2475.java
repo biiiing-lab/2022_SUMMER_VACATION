2b134cf86babc6c209292eb12c1e3f35adce7972
DESKTOP-A6KOF2G\youbi <youbinking@naver.com>
2022-07-31 11:28:39 +0900

    숫자 찾기

---
+++ Baekjoon_7/JAVA/Q2475.java
@@ -0,0 +1,19 @@
+package Baekjoon.JAVA;
+
+import java.util.Scanner;
+
+public class Q2475 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[5];
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            array[i] = sc.nextInt();
+            sum += array[i] * array[i];
+        }
+        System.out.println(sum % 10);
+    }
+}
